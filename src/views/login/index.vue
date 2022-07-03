4a1f85ed95e76a4f466e3e77ce08435f5aae5eb4
liulb <20224151@qq.com>
2022-07-03 18:07:08 +0800

    add2

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: liulb 20224151@qq.com
+ * @Date: 2022-07-03 17:13:06
+ * @LastEditors: liulb 20224151@qq.com
+ * @LastEditTime: 2022-07-03 18:05:11
+ * @FilePath: \vue-elementui-demo3\src\views\login\index.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
 	<div class="login">
 		<el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
@@ -10,7 +18,7 @@
 		    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
 		  </el-form-item>
 		  
-		  
+		   <el-button>确认</el-button>
 		</el-form>
 	</div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: liulb 20224151@qq.com
+ * @Date: 2022-07-03 13:53:29
+ * @LastEditors: liulb 20224151@qq.com
+ * @LastEditTime: 2022-07-03 18:00:18
+ * @FilePath: \vue-elementui-demo3\vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -5,6 +13,6 @@ module.exports = defineConfig({
   devServer:{
 	  port:8087,
 	  host:"localhost",
-	  //open:true
+	  open:true
   }
 })
